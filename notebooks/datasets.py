948bbe690e95db3a3809418090573369d3468313
Daniel Pfrommer <dan.pfrommer@gmail.com>
2025-04-21 10:40:58 -0400

    Adding oneway function based dataset

--- notebooks/datasets.py
+++ notebooks/datasets.py
@@ -1,41 +1,41 @@
+
+
 import marimo
 
-__generated_with = "0.12.10"
+__generated_with = "0.13.0"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
-    from nanodiffusion.datasets.trajectory import TrajectoryDataConfig
-    from nanodiffusion.datasets.tree  import TreeDataConfig
-    # dataset, _ = TrajectoryDataConfig().create()
-    dataset, _ = TreeDataConfig().create()
-    return TrajectoryDataConfig, TreeDataConfig, dataset
-
-
-@app.cell
-def _(dataset):
     from torch.utils.data import DataLoader
-
-    _batch = next(iter(DataLoader(dataset, shuffle=True, batch_size=2048)))
-    dataset.visualize_batch(_batch)
-    return (DataLoader,)
+    def visualize(config, N):
+        dataset, _ = config.create()
+        _batch = next(iter(DataLoader(dataset, shuffle=True,
+                                      batch_size=N)))
+        return dataset.visualize_batch(_batch)
+    return (visualize,)
 
 
 @app.cell
-def _(DataLoader, dataset):
-    loader = DataLoader(dataset, batch_size=1024, shuffle=True)
-    import time
-    t = time.time()
-    iteration = 0
-    for e in range(100):
-        for batch in loader:
-            batch.to("cuda")
-            iteration += 1
-    took = time.time() - t
-    print(iteration / took, "iter/second", "total", iteration, took, "seconds")
-    del e
-    return batch, e, iteration, loader, t, time, took
+def _(visualize):
+    from nanodiffusion.datasets.tree  import TreeDataConfig
+    visualize(TreeDataConfig(), 2048)
+    return
+
+
+@app.cell
+def _(visualize):
+    from nanodiffusion.datasets.trajectory import TrajectoryDataConfig
+    visualize(TrajectoryDataConfig(), 32)
+    return
+
+
+@app.cell
+def _(visualize):
+    from nanodiffusion.datasets.oneway import OnewayDataConfig
+    visualize(OnewayDataConfig(), 2048)
+    return
 
 
 @app.cell
