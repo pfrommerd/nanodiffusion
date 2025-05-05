0d2dbf96c706b088b7735522f2a7af6247bc04d0
Daniel Pfrommer <dan.pfrommer@gmail.com>
2025-05-05 17:38:43 -0400

    Updated sweep configuration

--- notebooks/visualizations.py
+++ notebooks/visualizations.py
@@ -67,22 +67,31 @@ def _(np):
 def _(mnist_data, np, scipy):
     def calc_density(sample_points):
         dists = -np.sum(np.square(sample_points[:,None, :] - mnist_data[None,:, :]), axis=-1)
-        dists = dists/25
+        dists = dists/50
         log_pdfs = scipy.special.logsumexp(dists, axis=1)
         log_pdfs -= np.log(10)
         # log_pdfs = log_pdfs - scipy.special.logsumexp(log_pdfs)
         return np.exp(log_pdfs)
-    return (calc_density,)
+
+    def calc_smoothed(cond, values):
+        dists = -np.sum(np.square(cond[:,None, :] - cond[None,:, :]), axis=-1)
+        return scipy.special.logsumexp(dists, axis=1, b=values)
+    return calc_density, calc_smoothed
 
 
 @app.cell
-def _(calc_density, data, np):
-    def add_densities():
+def _(calc_density, calc_smoothed, data, np):
+    def transform_densities(data):
         new_data = data.copy(deep=False)
         cond = np.stack((data["condition_x"], data["condition_y"]), axis=-1)
+        for c in ["ddpm_ddim_dist"]:
+            print("Smoothing", c)
+            new_data[c] = calc_smoothed(cond, new_data[c].to_numpy())
+        print("Calculating density...")
         new_data["density"] = calc_density(cond)
+        # average distances over conditioning points
         return new_data
-    transformed_data = add_densities()
+    transformed_data = transform_densities(data)
     return (transformed_data,)
 
 
@@ -101,6 +110,12 @@ def _(calc_density, np, plt):
 
 @app.cell
 def _(plt, transformed_data):
+    plt.scatter(transformed_data["condition_x"], transformed_data["condition_y"], c=transformed_data["ddpm_ddim_dist"])
+    return
+
+
+@app.cell
+def _(plt, transformed_data):
     plt.hist(transformed_data.density, bins=50)
     plt.show()
     return
@@ -111,15 +126,21 @@ def _(pd, plt, transformed_data):
     def _():
         fig, ax = plt.subplots()
         for samples, samples_data in transformed_data.groupby("samples"):
+            if samples > 50_000:
+                continue
             samples_data = samples_data.copy(deep=False)
+            samples_data = samples_data[samples_data["density"] > 0.5]
             density = samples_data["density"].to_numpy()
             min_density, max_density = density.min(), density.max()
             labels, bins = pd.cut(samples_data["density"], 10, retbins=True)
             bins = (bins[:-1] + bins[1:])/2
             samples_data["bins"] = labels
             data = samples_data.groupby("bins", observed=True).median().reset_index()
+            data_upper = samples_data.groupby("bins", observed=True).quantile(0.90).reset_index()
+            data_lower = samples_data.groupby("bins", observed=True).quantile(0.25).reset_index()
             data.bins = data.bins.apply(lambda x: (x.left + x.right)/2)
             ax.plot(data.bins, data.ddpm_ddim_dist, label=f"N={samples}")
+            ax.fill_between(data.bins, data_lower.ddpm_ddim_dist, data_upper.ddpm_ddim_dist, alpha=0.2)
         ax.set_xlabel("Density")
         ax.set_ylabel("DDPM/DDIM Transport Distance")
         fig.legend(loc="upper left")
